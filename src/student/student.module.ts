7a653e67c2f508d9d46f112e45ed8d1c709cbec5
Satyajit S K <99947701+Dragnias@users.noreply.github.com>
2023-03-17 15:21:59 +0530

    Update student.module.ts

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -4,6 +4,7 @@ import { StudentService } from './student.service';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { Student } from '../entities/student';
 @Module({
+  //typeorm module is imported for student entity
   imports:[TypeOrmModule.forFeature([Student])],
   controllers: [StudentController],
   providers: [StudentService]

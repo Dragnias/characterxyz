3e41b24e8df55345ab38f6d69bd11ac006480cf4
Satyajit S K <99947701+Dragnias@users.noreply.github.com>
2023-03-17 15:53:16 +0530

    Update student.ts

--- src/entities/student.ts
+++ src/entities/student.ts
@@ -1,9 +1,12 @@
+//necessary decorators are imported from typeorm module for creation of table
 import { Entity,PrimaryGeneratedColumn,Column } from "typeorm";
+//Entity decorator defines the table name
 @Entity({name:'Students'})
-export class Student{
+export class Student {
+    //PrimaryGeneratedColumn defines the primary key with auto increment
     @PrimaryGeneratedColumn({type:'bigint'})
     id:number;
-
+    //Column decorator defines the columns of the table
     @Column()
     name:string;
     
